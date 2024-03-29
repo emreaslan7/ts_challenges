555bbd971eb0b7365350018771c67f8e79e9b14d
emreaslan <emreaslan7@icloud.com>
2024-03-30 00:22:55 +0300

    if-00268 & concat-00533

--- easy/00043-easy-exclude.ts
+++ easy/00043-easy-exclude.ts
@@ -12,4 +12,13 @@ type cases = [
 // ============= Your Code Here =============
 type MyExclude<T, U> = T extends U ? never : T;
 
-const excluded: MyExclude<"a" | "b", "D" | "c"> = "b";
+// explaination of the MyExclude type
+// 1. MyExclude is a generic type that takes two type parameters T and U
+// 2. T extends U checks if T extends U
+// 3. if T extends U, then never is returned
+// 4. if T does not extend U, then T is returned
+
+// what is the role of never here?
+// never is a type that represents the type of values that never occur
+// it is used to represent the absence of a value
+// in this case, never is used to exclude a type from another type
