8fd793fd3d704acc67dcef070ca241b3b6ca4468
emreaslan <emreaslan7@icloud.com>
2024-04-07 00:28:26 +0300

    readonly-2-00008 & deep-readonly-00009

---
+++ medium/00008-medium-readonly-2.ts
@@ -0,0 +1,34 @@
+// ============= Test Cases =============
+import type { Alike, Expect } from "../.typeChallenges/test-utils";
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "description">, Expected>>
+];
+
+interface Todo1 {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
+}
+
+// ============= Your Code Here =============
+
+type MyReadonly2<T, K> = {
+  readonly [key in keyof T as key extends K ? key : never]: T[key];
+} & {
+  [key in keyof T as key extends K ? never : key]: T[key];
+};
