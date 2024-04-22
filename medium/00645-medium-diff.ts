7fc7662ccf0e31d003f1873ff319a50915705009
emreaslan <emreaslan7@icloud.com>
2024-04-23 00:20:25 +0300

    kebabcase-00612 & diff-00645

---
+++ medium/00645-medium-diff.ts
@@ -0,0 +1,32 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../.typeChallenges/test-utils";
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+type Coo = {
+  name: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+];
+
+// ============= Your Code Here =============
+type Diff<O, O1> = {
+  [K in keyof (O & O1) as K extends keyof O
+    ? K extends keyof O1
+      ? never
+      : K
+    : K]: (O & O1)[K];
+};
