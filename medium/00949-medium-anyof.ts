b25154eb84947777402b9d8d83888efd8217d0b7
emreaslan <emreaslan7@icloud.com>
2024-04-23 11:06:50 +0300

    anyof-00949

---
+++ medium/00949-medium-anyof.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../.typeChallenges/test-utils";
+
+type cases = [
+  Expect<
+    Equal<AnyOf<[1, "test", true, [1], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[1, "", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "test", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { 1: "test" }]>, true>>,
+  Expect<
+    Equal<AnyOf<[0, "", false, [], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[0, "", false, [], {}, undefined, null]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>
+];
+
+// ============= Your Code Here =============
+type Falses = 0 | "" | false | [] | null | undefined | { [key: string]: never };
+
+type AnyOf<T extends readonly any[]> = T[number] extends Falses ? false : true;
