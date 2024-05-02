bf1e8c4eebae38e512e975d58aaa43a49f176b01
emreaslan <emreaslan7@icloud.com>
2024-05-02 11:07:45 +0300

    mutable-02793

---
+++ medium/02793-medium-mutable.ts
@@ -0,0 +1,25 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../.typeChallenges/test-utils";
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+  meta: {
+    author: string;
+  };
+}
+
+type List = [1, 2, 3];
+
+type cases = [
+  Expect<Equal<Mutable<Readonly<Todo1>>, Todo1>>,
+  Expect<Equal<Mutable<Readonly<List>>, List>>
+];
+
+type errors = [Mutable<"string">, Mutable<0>];
+
+// ============= Your Code Here =============
+type Mutable<T> = {
+  -readonly [K in keyof T]: T[K];
+};
