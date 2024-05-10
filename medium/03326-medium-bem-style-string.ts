13309da37071e77bced03093dd2f5f8086d7457b
emreaslan7 <emreaslan7@icloud.com>
2024-05-10 11:47:10 +0300

    bem-style-string-03326

---
+++ medium/03326-medium-bem-style-string.ts
@@ -0,0 +1,29 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../.typeChallenges/test-utils";
+
+type cases = [
+  Expect<Equal<BEM<"btn", ["price"], []>, "btn__price">>,
+  Expect<
+    Equal<
+      BEM<"btn", ["price"], ["warning", "success"]>,
+      "btn__price--warning" | "btn__price--success"
+    >
+  >,
+  Expect<
+    Equal<
+      BEM<"btn", [], ["small", "medium", "large"]>,
+      "btn--small" | "btn--medium" | "btn--large"
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type BEM<B extends string, E extends string[], M extends string[]> =
+  | E
+  | M extends []
+  ? B
+  : E extends []
+  ? `${B}--${M[number]}`
+  : M extends []
+  ? `${B}__${E[number]}`
+  : `${B}__${E[number]}--${M[number]}`;
