ecc013f4de44113cdd4cd56576dd66b40c4a5fcc
emreaslan7 <emreaslan7@icloud.com>
2024-05-17 12:54:52 +0300

    istuple-04484

---
+++ medium/04484-medium-istuple.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../.typeChallenges/test-utils";
+
+type cases = [
+  Expect<Equal<IsTuple<[]>, true>>,
+  Expect<Equal<IsTuple<[number]>, true>>,
+  Expect<Equal<IsTuple<readonly [1]>, true>>,
+  Expect<Equal<IsTuple<{ length: 1 }>, false>>,
+  Expect<Equal<IsTuple<number[]>, false>>,
+  Expect<Equal<IsTuple<never>, false>>
+];
+
+// ============= Your Code Here =============
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends [any] | [] | readonly [any]
+  ? true
+  : false;
